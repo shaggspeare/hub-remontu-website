86f3d62330bc51e640af88a146a665f1f06be388
Slava Stetsenko <syavast@ukr.net>
2025-05-29 13:29:24 +0300

    add meta pixel code

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -16,6 +16,7 @@ import "../../../public/css/responsive.scss";
 
 import AosAnimation from "@/components/Layout/AosAnimation";
 import BackToTop from "@/components/Layout/BackToTop";
+import Script from "next/script";
 
 import { Geologica } from "next/font/google";
 
@@ -28,6 +29,33 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1387205899141887');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            alt="facebook pixel"
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1387205899141887&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </head>
       <body className={font.className}>
         {children}
 
