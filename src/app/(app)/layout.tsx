ff502bf03776f9b2dc8254384e4a657d5ca877b5
Slava Stetsenko <syavast@ukr.net>
2025-05-30 12:29:16 +0300

    added gtm and ga4

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -30,6 +30,31 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WBNG36TB');
+          `}
+        </Script>
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-DB220HDS6Y"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-DB220HDS6Y');
+          `}
+        </Script>
+
         {/* Meta Pixel Code */}
         <Script id="meta-pixel" strategy="afterInteractive">
           {`
@@ -45,6 +70,7 @@ export default function RootLayout({
             fbq('track', 'PageView');
           `}
         </Script>
+
         <noscript>
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
@@ -57,6 +83,16 @@ export default function RootLayout({
         </noscript>
       </head>
       <body className={font.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WBNG36TB"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         {children}
 
         {/* AosAnimation */}
